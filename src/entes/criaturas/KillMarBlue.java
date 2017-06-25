fea65727944710030bb0edf0e2ca277ce6367d38
CarlosDubon <carlosminerodubon@gmail.com>
2017-06-25 10:49:49 -0600

    a

--- src/entes/criaturas/KillMarBlue.java
+++ src/entes/criaturas/KillMarBlue.java
@@ -5,10 +5,58 @@
  */
 package entes.criaturas;
 
+import graficos.Pantalla;
+import graficos.Sprite;
+import mapa.Mapa;
+
 /**
  *
  * @author Carlos
  */
-public class KillMarBlue {
+public class KillMarBlue extends Criatura {
+    private Jugador jugador;
     
+    public KillMarBlue(int x, int y,Sprite sprite, Jugador jugador, Mapa mapa){
+        this.x = x;
+        this.y = y;
+        this.sprite = sprite;
+        this.jugador = jugador;
+        this.mapa = mapa;
+        this.HP = 60;
+        this.Velocidad = 2;
+        this.originalX = x;
+        this.originalY = y;
+        this.isKiller = false;
+        this.ATK = 3;
+    }
+    public void actualizar(){
+        desplazamientoX =0;
+        desplazamientoY =0;
+        
+        if(animacion < 32767){
+            animacion++;
+        }else{
+            animacion =0;
+        }
+        
+        if(HP<=0 && !isKiller){
+            jugador.setPuntaje(jugador.getPuntaje()+45);
+            isKiller = true;
+        }
+        
+        if (HP > 0){
+            isMove = true;
+        }else{
+            isMove = false;
+        }
+        
+        if(isMove){
+        
+        }else{
+        
+        }
+    }
+    public void mostrar(Pantalla pantalla){
+    
+    }
 }
